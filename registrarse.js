a21c73a67f0e209ebb2a566fb3863b6aac29e9d3
EmilianoGDY <emilianoo9g@gmail.com>
2024-06-22 00:06:55 -0300

    finalizado

--- registrarse.js
+++ registrarse.js
@@ -1,30 +1,33 @@
-const nombre = document.getElementById("name")
-const apellido = document.getElementById("surname")
-const usuario = document.getElementById("user")
-const email = document.getElementById("email")
-const contrasena = document.getElementById("pass")
-const parrafo = document.getElementById("warnings")
-const form = document.getElementById("form")
 
+function registrar(){ 
 
-function registrar(){ e=>{
+    const nombre = document.getElementById("name")
+    const apellido = document.getElementById("surname")
+    const usuario = document.getElementById("user")
+    const email = document.getElementById("email")
+    const contrasena = document.getElementById("pass")
+    const parrafo = document.getElementById("warnings")
+    const form = document.getElementById("form")
     
     let warnings = ''
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     let entrar = false
+    
     if (nombre.value.length < 6){
         warnings += `El nombre no es valido <br>`
         entrar = true
     }
-    console.log(regexEmail.test(email.value))
+    
     if(!regexEmail.test(email.value)){
         warnings += `El email no es valido <br>`
         entrar = true
     }
 
-    if(contrasena.value.lenght < 8){
-        warnings += `La contrasenia no es valida`
+    
+    if(contrasena.value.length < 8){
+        warnings += `La contraseña no es valida`
         entrar = true
+        
     }
 
     if(entrar){
@@ -32,5 +35,5 @@ function registrar(){ e=>{
     }else{
         parrafo.innerHTML = "Enviado"
     }
-    form.submit()
-} }
+    
+} 
